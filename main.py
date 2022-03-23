76bb69ec08b76b72e7b3d0ececde3e8f8dc8e5a7
Egor Zhukov <bit.zhukov@gmail.com>
2022-03-23 16:51:27 +0300

    finished

--- main.py
+++ main.py
@@ -1,16 +1,129 @@
-# This is a sample Python script.
+import argparse
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Agent import Agent, ExperienceBuffer
+import wrappers
+from dqn_model import DQN
+import numpy as np
+
+import torch
+import torch.nn as nn
+from tensorboardX import SummaryWriter
+import torch.optim as optim
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+DEFAULT_ENV_NAME = "PongNoFrameskip-v4"
+MEAN_REWARD_BOUND = 19.5
+
+GAMMA = 0.99
+BATCH_SIZE = 32
+REPLAY_SIZE = 10000
+LR = 1e-4
+SYNC_TARGET_FRAMES = 1000
+REPLAY_START_SIZE = 10000
+
+EPSILON_DECAY_LAST_FRAME = 10**5
+EPSILON_START = 1.0
+EPSILON_FINAL = 0.02
 
 
-# Press the green button in the gutter to run the script.
+def calc_loss(batch, net, tgt_net, device=torch.device('cpu')):
+    """
+
+    :param batch: батч векторов
+    :param net: сеть обучамая оптимизации Q функции
+    :param tgt_net: сеть для вычисления следующих состояний
+    :param device:
+    :return:
+    """
+    states, actions, rewards, dones, next_states = batch
+
+    # оборачиваем все в тензоры
+    states_v = torch.tensor(states).to(device)
+    next_states_v = torch.tensor(next_states).to(device)
+    actions_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).to(device)
+    done_mask = torch.ByteTensor(dones).to(device)
+
+    # получаем состояния по индексу действия
+    state_action_values = net(states_v).gather(1, actions_v.unsqueez(-1)).squeeze(-1)
+    # получаем будущие состояние по максимальному значению Q функции
+    next_state_values = tgt_net(next_states_v).max(1)[0]
+    # финальное состояние отсутствует поэтому зануляем его
+    next_state_values[done_mask] = 0.0
+    # разрываем связь значений с градиентом, чтобы они не утекли со второй сети на первую
+    next_state_values = next_state_values.detach()
+    # вычисляем значение Q функции
+    expected_state_action_values = next_state_values * GAMMA + rewards_v
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("--env", default=DEFAULT_ENV_NAME,
+                        help="Name of the environment, default=" + DEFAULT_ENV_NAME)
+    parser.add_argument("--reward", type=float, default=MEAN_REWARD_BOUND,
+                        help="Mean reward boundary for stop of training, default=%.2f" % MEAN_REWARD_BOUND)
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    env = wrappers.make_env(args.env)
+
+    net = DQN(env.observation_space.shape, env.action_space.n).to(device)
+    tgt_net = DQN(env.observation_space.shape, env.action_space.n).to(device)
+    writer = SummaryWriter(comment="-" + args.env)
+    print(net)
+
+    buffer = ExperienceBuffer(REPLAY_SIZE)
+    agent = Agent(env, buffer)
+    epsilon = EPSILON_START
+
+    optimizer = optim.Adam(net.parameters(), lr=LR)
+    total_rewards = []
+    frame_idx = 0
+    ts_frame = 0
+    ts = time.time()
+    best_mean_reward = None
+
+    while True:
+        frame_idx += 1
+        epsilon = max(EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME)
+
+        reward = agent.play_step(net, epsilon, device=device)
+        if reward is not None:
+            total_rewards.append(reward)
+            speed = (frame_idx - ts_frame) / (time.time() - ts)
+            ts_frame = frame_idx
+            ts = time.time()
+            mean_reward = np.mean(total_rewards[-100:])
+            print("%d: done %d games, mean reward %.3f, eps %.2f, speed %.2f f/s" % (
+                frame_idx, len(total_rewards), mean_reward, epsilon,
+                speed
+            ))
+            writer.add_scalar("epsilon", epsilon, frame_idx)
+            writer.add_scalar("speed", speed, frame_idx)
+            writer.add_scalar("reward_100", mean_reward, frame_idx)
+            writer.add_scalar("reward", reward, frame_idx)
+            if best_mean_reward is None or best_mean_reward < mean_reward:
+                torch.save(net.state_dict(), args.env + "-best.dat")
+                if best_mean_reward is not None:
+                    print("Best mean reward updated %.3f -> %.3f, model saved" % (best_mean_reward, mean_reward))
+                best_mean_reward = mean_reward
+            if mean_reward > args.reward:
+                print("Solved in %d frames!" % frame_idx)
+                break
+
+        if len(buffer) < REPLAY_START_SIZE:
+            continue
+
+        if frame_idx % SYNC_TARGET_FRAMES == 0:
+            tgt_net.load_state_dict(net.state_dict())
+
+        optimizer.zero_grad()
+        batch = buffer.sample(BATCH_SIZE)
+        loss_t = calc_loss(batch, net, tgt_net, device=device)
+        loss_t.backward()
+        optimizer.step()
+    writer.close()
+
